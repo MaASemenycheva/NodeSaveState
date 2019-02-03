cd7c758f86a803d876ab62ca3dae5c43ce35656a
Semenycheva Maria <you@example.com>
2019-02-03 10:29:16 -0500

    adding authentication

--- djangoneurobot/urls.py
+++ djangoneurobot/urls.py
@@ -25,5 +25,5 @@ router.register(r'state', views.StateViewSet)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(router.urls)),
-    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- nodesavestate/views.py
+++ nodesavestate/views.py
@@ -2,11 +2,11 @@ from nodesavestate.models import State
 from nodesavestate.serializers import StateSerializer
 
 from rest_framework import viewsets
-#from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 
 
 
 class StateViewSet(viewsets.ModelViewSet):
     queryset = State.objects.all()
     serializer_class = StateSerializer
-    #permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
