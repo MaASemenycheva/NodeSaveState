4f047879b7d12552336289313b5b538f48d5ad7e
Semenycheva Maria <you@example.com>
2019-02-03 16:58:25 -0500

    created share authentication

--- djangoneurobot/settings.py
+++ djangoneurobot/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_swagger',
-    'nodesavestate'
+    'nodesavestate',
+    'shares'
 ]
 
 MIDDLEWARE = [
--- nodesavestate/views.py
+++ nodesavestate/views.py
@@ -3,6 +3,7 @@ from nodesavestate.serializers import StateSerializer
 
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 
 
 
